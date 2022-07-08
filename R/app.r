d6d0067e9818849773c736627999f22c3097320b
Gregg Whitworth <whitworthg@wlu.edu>
2022-07-08 10:28:30 -0400

    possible dependency fix

--- R/app.r
+++ R/app.r
@@ -3,11 +3,30 @@ library(shiny)
 
 ui <- fluidPage(
 
-  p("Hello.")
+  p("Hello."),
+  textInput("message", label = "Message"),
+  actionButton("submit", "Submit"),
+  textOutput("status"),
+  actionButton("update", "Update")
     
 )
 
 server <- function(session, input, output) {
+
+  if (!file.exists("messages.log")) {
+    cat("Messages:\n", file = "messages.log")
+  }
+  
+  observeEvent(input$submit, {
+    cat(paste0(input$message, "\n"), file = "messages.log", append = TRUE)
+    updateTextInput(session, "message", value = "")
+  })
+  
+  output$status <- renderText({
+    input$update
+    print(readLines("messages.log"))
+    paste(readLines("messages.log"), sep = "\n")
+  })
 
 }
 
