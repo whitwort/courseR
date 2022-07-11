4dcc0685f1ec762dfab28ede9f2f9888d94d5ffc
Gregg Whitworth <whitworthg@wlu.edu>
2022-07-11 09:37:42 -0400

    still more experiments

--- R/connect-app.r
+++ R/connect-app.r
@@ -1,14 +1,32 @@
 connectUI <- fluidPage(
   
   p("Hello."),
-  textOutput("files")
+  textAreaInput("message", "Message"),
+  actionButton("submit", "Submit"),
+  textOutput("messages"),
+  actionButton("reload", "Reload")
   
 )
 
+
+
 connectServer <- function(session, input, output) {
   
-  output$files <- renderText({
-    list.files()
+  observeEvent(input$submit, {
+    if (!file.exists("messages.log")) {
+      cat("\n", file = "messages.log")
+    }
+    
+    cat(input$message, "\n", file = "messages.log", append = TRUE)
+    updateTextInput(session, "message", value = "")
+    
+  })
+  
+  output$messages <- renderText({
+    input$reload
+    if (file.exists("messages.log")) {
+      readLines("messages.log")
+    }
   })
   
 }
